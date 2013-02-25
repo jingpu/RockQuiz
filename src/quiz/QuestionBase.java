1b83ea4b50653a7e40f8097a8285f141f23575d9
Jing Pu <jimmypu07@gmail.com>
2013-02-24 21:11:09 -0800

    Completed MyQuiz constructor from database

--- src/quiz/QuestionBase.java
+++ src/quiz/QuestionBase.java
@@ -1,6 +1,5 @@
 package quiz;
 
-
 public abstract class QuestionBase {
 	protected final String questionType;
 	protected final String questionId;
@@ -8,8 +7,6 @@ public abstract class QuestionBase {
 	protected final String questionDescription;
 	protected final String answer;
 	protected final String maxScore;
-	
-	
 
 	public QuestionBase(String questionType, String questionId,
 			String typeIntro, String questionDescription, String answer,
@@ -22,46 +19,49 @@ public abstract class QuestionBase {
 		this.answer = answer;
 		this.maxScore = maxScore;
 	}
-	
-	public static QuestionBase getQuestion(String questionType, String questionId){
-		//switch questionType
+
+	public static QuestionBase getQuestion(String questionType,
+			String questionId) {
+		// switch questionType
 		// case A: call questionAConstructor();
-		//return QuestionBase   //factory?/
-		switch(questionType) {
-			case A: return new typeA(questionId);
-			case B: return new typeB(questionId);
-		}
+		// return QuestionBase //factory?/
+		/*
+		 * switch(questionType) { case A: return new typeA(questionId); case B:
+		 * return new typeB(questionId); }
+		 */
+		return null;
 	}
 
-	//when clicking submit
-	public void saveToDatabase(){
+	// when clicking submit
+	public void saveToDatabase() {
 	}
-	
-	
+
 	public abstract String printHTML();
-    
-	
+
 	public abstract String getScore(String userInput);
-	
-	
+
 	public String getMaxScore() {
 		return maxScore;
 	}
-	
+
 	public String getQuestionType() {
 		return questionType;
 	}
+
 	public String getQuestionId() {
 		return questionId;
 	}
+
 	public String getTypeIntro() {
 		return typeIntro;
 	}
+
 	public String getQuestionDescription() {
 		return questionDescription;
 	}
+
 	public String getAnswer() {
 		return answer;
 	}
-	
+
 }
---
+++ src/util/Helper.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.List;
+
+public final class Helper {
+
+	public static String generateTags(List<String> tags) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static List<String> parseTags(String tagString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
