433bba8c45811888493a20f9dede54c2694e160a
Jason Zhao <yzhao3@stanford.edu>
2013-02-25 00:53:10 -0800

    Create the html and jsp structures, but haven't tested

--- src/util/Helper.java
+++ src/util/Helper.java
@@ -1,16 +1,24 @@
 package util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Helper {
 
 	public static String generateTags(List<String> tags) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder s = new StringBuilder();
+		for (String t : tags) {
+			s.append("#" + t);
+		}
+		return s.toString();
 	}
 
 	public static List<String> parseTags(String tagString) {
-		// TODO Auto-generated method stub
-		return null;
+		String[] splits = tagString.split("#");
+		List<String> list = new ArrayList<String>(splits.length);
+		for (int i = 0; i < splits.length; i++)
+			if (!splits[i].equals(""))
+				list.add(splits[i]);
+		return list;
 	}
 }
