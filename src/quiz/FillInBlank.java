e952c24859b3d142d6df721ba39ab402aee8edd7
Jason Zhao <yzhao3@stanford.edu>
2013-02-25 23:54:41 -0800

    finish html structure in both creat and read mode

--- src/quiz/FillInBlank.java
+++ src/quiz/FillInBlank.java
@@ -3,17 +3,28 @@
  */
 package quiz;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-
-import database.MyDB;
 
 /**
  * @author yang
  *
  */
 public class FillInBlank extends QuestionBase {
+	private static final String typeIntro = "In this type of question, given a question with a blank, user need to fill in the blank";
+	
+	
+	
+	
+	public FillInBlank(String questionType, String creatorId,
+			String questionDescription, String answer, String maxScore,
+			String tagString, String correctRatio) {
+		super(questionType, creatorId, questionDescription, answer, maxScore,
+				tagString, correctRatio);
+			
+		// TODO Auto-generated constructor stub
+	}
 
+	
 	public FillInBlank(String questionType, String questionId) {
 		// TODO Auto-generated constructor stub
 		super(questionType, questionId);
@@ -48,15 +59,22 @@ public class FillInBlank extends QuestionBase {
 	public String getScore(String userInput) {
 		// TODO Auto-generated method stub
 		if (userInput.equals(answer)) return  maxScore;
-		return null;
+		return "0";
 	}
 	
 	
-	@Override
-	public String printCreateHtml() {
+	private String parsePrefix() {
+		return questionDescription.split("#blank#")[0];
+	}
+	
+	private String parseSuffix() {
+		return questionDescription.split("#blank#")[1];
+	}
+	
+	
+	public static String printCreateHtml() {
 		// TODO Auto-generated method stub
 		StringBuilder html = new StringBuilder();
-		html.append(super.printCreateHtml());
 		html.append("<h1>This page will guide you to create a Fill-In-Blank question</h1>");
 		html.append("<p> Please enter proposed question description and answer. In order to insert a blank, please follow the format #blank# </p>");
 		
@@ -66,17 +84,16 @@ public class FillInBlank extends QuestionBase {
 		html.append("<p>Question Description: <textarea name=\"questionDescription\" rows=\"10\" cols=\"50\"></textarea></p>");
 		html.append("<p>Answer:   <input type=\"text\" name=\"answer\" ></input></p>");
 		html.append("<p>Score:   <input type=\"text\" name=\"maxScore\" ></input></p>");
+		
+		//Hidden information - question Type and tag information
 		html.append("<p><input type=\"hidden\" name=\"questionType\" ></input></p>");
-		html.append("<p><input type=\"hidden\" name=\"questionId\" ></input></p>");
+		html.append("<p><input type=\"hidden\" name=\"tag\" ></input></p>");
 		html.append("<input type=\"submit\" value = \"Save\"/></form>");
 		html.append("");
 
 		return html.toString();
 	}
 
-	
-	  
-	
 	
 	
 	@Override
@@ -88,8 +105,14 @@ public class FillInBlank extends QuestionBase {
 		html.append("<h1>This is a question page, please read the question information, and make an answer</h1>");
 		html.append("<form action=\"QuizCreationServlet\" method=\"post\">");
 		html.append("<p>Question Description: ");
-		html.append(questionDescription + "</p>");
-		html.append("<p>Answer:   <input type=\"text\" name=\"answer\" ></input></p>");
+		
+		//create prefix, blank, suffix
+		html.append(parsePrefix());
+		html.append("<p><input type=\"text\" name=\"answer\" ></input></p>");
+		html.append(parseSuffix());
+		html.append("</p>");
+		
+		//Hidden information - questionType and questionId information
 		html.append("<p><input type=\"hidden\" name=\"questionType\" ></input></p>");
 		html.append("<p><input type=\"hidden\" name=\"questionId\" ></input></p>");
 		html.append("<input type=\"submit\" value = \"Next\"/></form>");
