0fc5b296c7347048794345203a1f730043cab63f
Jing Pu <jimmypu07@gmail.com>
2013-02-28 02:06:03 -0800

    merged test_quiz_for)jing.sql into the Global sql file

--- src/sql/Global_Static_Table.sql
+++ src/sql/Global_Static_Table.sql
@@ -27,7 +27,39 @@ INSERT INTO Global_Quiz_Info_Table VALUES
 							   User should follow the instruction to complete the quiz",
 							   "#His", false, true, false, false,"2012-01-19 05:14:07");
 
+DROP TABLE IF EXISTS quizExample0_Content_Table;
+ -- remove table if it already exists and start from scratch
+
+CREATE TABLE quizExample0_Content_Table (  
+	questionNum CHAR(32),
+    questionType CHAR(32),
+    questionId CHAR(32)
+);
+
+INSERT INTO quizExample0_Content_Table VALUES
+	("0","Question_Response","0001"),
+    ("1","Fill_In_Blank","0001"),
+    ("2","Multi_Choice","0001");	
 							   
+DROP TABLE IF EXISTS quizExample0_Event_Table;
+ -- remove table if it already exists and start from scratch
+
+CREATE TABLE quizExample0_Event_Table (  
+	quizId CHAR(32),
+    userName CHAR(32),
+    submitTime TIMESTAMP,
+    timeElapsed BIGINT,
+    score INT
+);
+
+INSERT INTO quizExample0_Event_Table VALUES
+	("0","Patrick","2013-01-19 03:14:07", 60000, 8),
+    ("1","Molly","2013-01-19 03:14:07", 600000, 9),
+    ("2","Jim","2013-03-19 02:14:07", 50000, 10),
+    ("3","Kate","2013-03-19 05:14:07", 30000, 7),
+    ("4","Molly","2013-01-19 01:14:07", 10000, 6),
+    ("5","Molly","2013-03-19 05:14:07", 3000, 8),
+    ("6","Patrick","2013-02-19 03:14:07", 20000, 7);							   
 						
 
 DROP TABLE IF EXISTS Question_Response_Pool;
