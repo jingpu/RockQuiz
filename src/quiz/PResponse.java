8463ae6b0bb64b473ac89691b7a1216e2ee1db0d
Jason Zhao <yzhao3@stanford.edu>
2013-02-27 21:19:54 -0800

    fix all saveToDatabse query statement error

--- src/quiz/PResponse.java
+++ src/quiz/PResponse.java
@@ -7,20 +7,20 @@ import java.sql.SQLException;
 
 /**
  * @author yang
- *
+ * 
  */
 public class PResponse extends QuestionBase {
 	private String url;
 	private static final String typeIntro = "In this type of question, given a picture, user need to answer the related question";
-	
-	
-	public PResponse(String questionType, String creatorId, String questionDescription, String answer,
-			String maxScore, String tagString, String correctRation, String url) {
-		super(questionType, creatorId, questionDescription, answer, maxScore, tagString, correctRation);
+
+	public PResponse(String questionType, String creatorId,
+			String questionDescription, String answer, String maxScore,
+			String tagString, String correctRation, String url) {
+		super(questionType, creatorId, questionDescription, answer, maxScore,
+				tagString, correctRation);
 		// TODO Auto-generated constructor stub
 		this.url = url;
 	}
-
 
 	public PResponse(String questionType, String questionId) {
 		super(questionType, questionId);
@@ -35,31 +35,24 @@ public class PResponse extends QuestionBase {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	@Override
 	public void saveToDatabase() {
 		// TODO Auto-generated method stub
-		queryStmt = "INSERT INTO " + PR_Table + "VALUES (\"" +
-		questionId + "\", \"" +
-		creatorId + "\", \"" +
-		typeIntro + "\", \"" +
-		questionDescription  + "\", \"" +
-		answer + "\", \"" +
-		maxScore + "\", \"" +
-		tagString + "\", \"" +
-		correctRatio + "\", \"" +
-		url + "\")";
+		queryStmt = "INSERT INTO " + PR_Table + " VALUES (\"" + questionId
+				+ "\", \"" + creatorId + "\", \"" + typeIntro + "\", \""
+				+ questionDescription + "\", \"" + answer + "\", \"" + maxScore
+				+ "\", \"" + tagString + "\", \"" + correctRatio + "\", \""
+				+ url + "\")";
 
 		try {
-		rs = stmt.executeQuery(queryStmt);
+			stmt.executeUpdate(queryStmt);
 		} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
-	
 	public static String printCreateHtml() {
 		// TODO Auto-generated method stub
 		StringBuilder html = new StringBuilder();
@@ -67,36 +60,37 @@ public class PResponse extends QuestionBase {
 		html.append("<form action=\"QuizCreationServlet\" method=\"post\">");
 		html.append("<p> Please enter proposed question description and answer </p>");
 		html.append("<p>Question Description: <textarea name=\"questionDescription\" rows=\"10\" cols=\"50\"></textarea></p>");
-		
-		//url information
+
+		// url information
 		html.append("<p>Picture URL: <input type=\"text\" name=\"url\" ></input></p>\n");
 		html.append("<p>Answer:   <input type=\"text\" name=\"answer\" ></input></p>");
 		html.append("<p>Score:   <input type=\"text\" name=\"maxScore\" ></input></p>");
-		
-		//Hidden information - question Type and tag information
+
+		// Hidden information - question Type and tag information
 		html.append("<p><input type=\"hidden\" name=\"questionType\" value=\""
 				+ QuestionBase.PR + "\" ></input></p>");
 		html.append("<p><input type=\"hidden\" name=\"tag\" value=\"not_implemeted\"></input></p>");
 		html.append("<input type=\"submit\" value = \"Save\"/></form>");
 		return html.toString();
-		
+
 	}
-	
+
 	@Override
 	public String printReadHtml() {
 		// TODO Auto-generated method stub
 		StringBuilder html = new StringBuilder();
 		html.append(super.printReadHtml());
-		
+
 		html.append("<p>This is a question page, please read the question information, and make an answer</p>");
 		html.append("<form action=\"QuestionProcessServlet\" method=\"post\">");
 		html.append("<p>Question Description: ");
 		html.append(questionDescription + "</p>");
-		
-		html.append("<img border=\"0\" src=\"" + url + "\" width=\"304\" height=\"228\">");
+
+		html.append("<img border=\"0\" src=\"" + url
+				+ "\" width=\"304\" height=\"228\">");
 		html.append("<p>Answer:   <input type=\"text\" name=\"answer\" ></input></p>");
-		
-		//Hidden information - questionType and  questionId information
+
+		// Hidden information - questionType and questionId information
 		html.append("<p><input type=\"hidden\" name=\"questionType\" value=\""
 				+ getQuestionType() + "\"></input></p>");
 		html.append("<p><input type=\"hidden\" name=\"questionId\" value=\""
@@ -104,17 +98,19 @@ public class PResponse extends QuestionBase {
 		html.append("<input type=\"submit\" value = \"Next\"/></form>");
 
 		return html.toString();
-		
-	}
-	
 
-	/* (non-Javadoc)
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see quiz.QuestionBase#getScore(java.lang.String)
 	 */
 	@Override
 	public String getScore(String userInput) {
 		// TODO Auto-generated method stub
-		if (userInput.equals(answer)) return  maxScore;
+		if (userInput.equals(answer))
+			return maxScore;
 		return "0";
 	}
 
