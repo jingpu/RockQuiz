7c28610982866c0b1dee39bdd0cb8f7b7f493268
Jason Zhao <yzhao3@stanford.edu>
2013-03-14 20:58:29 -0700

    Revert "mis delete QuestionCreationServlet"
    This reverts commit 90a947cfb23ceb6eb1e7c19f51717a8cf47ea112.

--- WebContent/JavaScript/match_question.js
+++ WebContent/JavaScript/match_question.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
            {id: 0,text: $("#cs0").text()},
            {id: 1,text: $("#cs1").text()},
            {id: 2,text: $("#cs2").text()},
-           {id: 3,text: $("#cs3").text()},], 
+           {id: 3,text: $("#cs3").text()},],
         
         responses: [
            {id: 0,text: $("#as0").text()},
@@ -20,14 +20,10 @@ $(document).ready(function() {
 
     $("#result").click(function(){
         var res = $("#match").match("getValues");
-        for (var i = 0; i < res.length; i++) {
+        for (var i = 0; i < 4; i++) {
             var item = res[i];
             choiceId = "#choice" + i;
             $(choiceId).val(item.response.text);
-        }
-        for (var i = res.length; i < 4; i++) {
-        	  choiceId = "#choice" + i;
-              $(choiceId).val("");
         }
     });
     
